7bb9ad8ecbca14356a70f932c91b9288323e1cba
Sonya Gould <sonyavgould@gmail.com>
2020-06-12 04:33:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,33 @@
+# def square_array(array)
+#   new_array = []
+#   array.each do |i| 
+#   new_array << i**2
+# end
+#   new_array
+# end
+
+# # def square_array(array)
+# #   array.collect {|i| i**2}
+# # end
+
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+  while square_array[counter] do |integer|
+  new_array << integer**2
+  counter += 1
 end
+end
+
+# def square_array(array)
+#   length = array.length
+#   counter = 0
+#   new_array = []
+#   while counter <= length do
+#     new_array << array[counter]**2
+#     counter +=1
+#   end
+#   new_array
+# end
+
+# square_array(1,2,3)
