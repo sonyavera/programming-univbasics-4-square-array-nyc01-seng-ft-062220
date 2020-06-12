b0aba1f71a8118dba1e34e065cca955605530604
Sonya Gould <sonyavgould@gmail.com>
2020-06-12 04:35:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   counter = 0
-  while square_array[counter] |integer|
+  while square_array[counter] do |integer|
   new_array << integer**2
   counter += 1
   end
